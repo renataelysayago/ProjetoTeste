45b03184244d4f85cac3b5cc9a0b4209fa3342ad
Renata Ely <renataely@tecgraf.puc-rio.br>
2022-08-05 22:00:26 -0300

    [ProjetoTeste] Estrutura do back-end

---
+++ ProjetoTeste.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ProjetoTeste.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ ProjetoTeste.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ProjetoTeste.Domain
+{
+    public class Class1
+    {
+    }
+}
